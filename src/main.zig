050e5e8f6d99e2248e6e9a140ecb35b850391b99
Javier Orfo <javierorfo@prontonmail.com>
2024-12-28 18:57:10 -0300

    test

--- src/main.zig
+++ src/main.zig
@@ -37,13 +37,36 @@ fn threadBar(components: []Component) void {
         for (components) |comp| {
             finalStr = std.fmt.allocPrint(std.heap.page_allocator, "{s}{s}", .{ finalStr, comp.result }) catch "error";
         }
-        std.debug.print("{s}\n", .{finalStr});
+        //         std.debug.print("{s}\n", .{finalStr});
+        callXsetroot(finalStr) catch return;
 
         std.time.sleep(100 * std.time.ns_per_ms);
     }
 }
 
-fn uno(arg: []const u8) []const u8 {
+fn callXsetroot(str: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
+    const args = [_][]const u8{ "xsetroot", "-name", str };
+    var child = std.process.Child.init(&args, allocator);
+
+    child.stdin_behavior = .Ignore;
+    child.stdout_behavior = .Ignore;
+    child.stderr_behavior = .Inherit;
+
+    try child.spawn();
+    const term = try child.wait();
+
+    switch (term) {
+        .Stopped, .Signal, .Unknown => |err| std.debug.print("Command failed: {}\n", .{err}),
+        .Exited => return,
+    }
+}
+
+fn disk(arg: []const u8) []const u8 {
     const m = sys.disk.usage("/") catch {
         return "";
     };
@@ -51,19 +74,35 @@ fn uno(arg: []const u8) []const u8 {
     return std.fmt.allocPrint(std.heap.page_allocator, "{s} {d}% | ", .{ arg, p }) catch "error";
 }
 
-fn dos(arg: []const u8) []const u8 {
+fn ram(arg: []const u8) []const u8 {
+    const m = sys.memory.usage() catch {
+        return "";
+    };
+    const p = m.percentageUsed() catch 0;
+    return std.fmt.allocPrint(std.heap.page_allocator, "{s} {d:.0}% | ", .{ arg, p }) catch "error";
+}
+
+fn cpu(arg: []const u8) []const u8 {
     const p = sys.cpu.percentageUsed() catch 0;
     return std.fmt.allocPrint(std.heap.page_allocator, "{s} {d:.0}% | ", .{ arg, p }) catch "error";
 }
 
-fn cua(arg: []const u8) []const u8 {
+fn temp(arg: []const u8) []const u8 {
+    const p = sys.thermal.getTemperatureFromZone(sys.thermal.ZONE.two) catch 0;
+    return std.fmt.allocPrint(std.heap.page_allocator, "{s} {d:.0}% | ", .{ arg, p }) catch "error";
+}
+
+fn volume(arg: []const u8) []const u8 {
     const p = sys.volume.state(.{}) catch {
         return "error";
     };
-    return std.fmt.allocPrint(std.heap.page_allocator, "{s} {d}% | ", .{ arg, p.volume }) catch "error";
+    if (!p.muted) {
+        return std.fmt.allocPrint(std.heap.page_allocator, "  {s} {d}% | ", .{ arg, p.volume }) catch "error";
+    }
+    return std.fmt.allocPrint(std.heap.page_allocator, "󰖁  {s} MUTED | ", .{arg}) catch "error";
 }
 
-fn tres(arg: []const u8) []const u8 {
+fn date(arg: []const u8) []const u8 {
     const DATE_FORMAT = "%A %d/%m/%Y %H:%M:%S";
     var now: c.time_t = c.time(null);
     const local: *c.struct_tm = c.localtime(&now);
@@ -75,10 +114,12 @@ fn tres(arg: []const u8) []const u8 {
 
 pub fn main() !void {
     var components = [_]Component{
-        .{ .head = "DISK", .fun = uno, .time = 2000 },
-        .{ .head = "CPU", .fun = dos, .time = 1000 },
-        .{ .head = "VOL", .fun = cua, .time = 200 },
-        .{ .head = "", .fun = tres, .time = 1000 },
+        .{ .head = "  CPU", .fun = cpu, .time = 1000 },
+        .{ .head = "  RAM", .fun = ram, .time = 1000 },
+        .{ .head = "󰏈  TEMP", .fun = temp, .time = 1000 },
+        .{ .head = "󰋊  DISK", .fun = disk, .time = 2000 },
+        .{ .head = "VOL", .fun = volume, .time = 200 },
+        .{ .head = " ", .fun = date, .time = 1000 },
     };
 
     var threads: [components.len]std.Thread = undefined;
